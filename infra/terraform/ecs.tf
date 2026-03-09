1a5facf21ae9498af2a672a2e7956baf965f8218
rtokar <romantokar.biz@gmail.com>
2026-03-09 22:34:16 +0200

    Decreased deregistration delay and health check registration interval

--- infra/terraform/ecs.tf
+++ infra/terraform/ecs.tf
@@ -186,11 +186,12 @@ resource "aws_lb" "api" {
 
 # Target Group for ECS Service
 resource "aws_lb_target_group" "api" {
-  name        = "${var.app_name}-api-tg"
-  port        = 3001
-  protocol    = "HTTP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
+  name                       = "${var.app_name}-api-tg"
+  port                       = 3001
+  protocol                   = "HTTP"
+  vpc_id                     = aws_vpc.main.id
+  target_type                = "ip"
+  deregistration_delay       = 30
 
   health_check {
     enabled             = true
@@ -198,7 +199,7 @@ resource "aws_lb_target_group" "api" {
     port                = "traffic-port"
     protocol            = "HTTP"
     matcher             = "200"
-    interval            = 30
+    interval            = 15
     timeout             = 5
     healthy_threshold   = 2
     unhealthy_threshold = 3
